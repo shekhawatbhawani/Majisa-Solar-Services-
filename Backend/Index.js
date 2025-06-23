3d87cd4574ee7ffc5ae1952ea54f60667c223282
Shekhawatbhawanisingh <shekhawatbhwanisingh49@gmail.com>
2025-06-23 16:30:37 +0530

    fix: CORS issue for frontend domain

--- Backend/Index.js
+++ Backend/Index.js
@@ -18,6 +18,8 @@ const app = express();
 const corsOptions = {
   origin:  ['http://localhost:5173', 'https://majisa-solar.vercel.app'], // your frontend Vite dev server
   credentials: true,
+  
+  methods: ["GET", "POST", "PUT", "DELETE"]
 };
 
 app.use(cors(corsOptions));
