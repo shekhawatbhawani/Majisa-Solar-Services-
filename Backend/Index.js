8557f9b8ad4c6a6864f55353cc41517739e8b1a4
Shekhawatbhawanisingh <shekhawatbhwanisingh49@gmail.com>
2025-06-23 14:05:10 +0530

    fix: use process.env.PORT for Render

--- Backend/Index.js
+++ Backend/Index.js
@@ -36,7 +36,7 @@ app.use("/api", contactRoutes);
 app.use("/api/attendance", attendanceRoutes);
 app.use("/api/expenses", expenseRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => {
     console.log(`Server is running on port : ${PORT}`)
 })
