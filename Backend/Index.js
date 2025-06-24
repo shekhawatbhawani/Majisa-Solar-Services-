32c9c8c845f8f05e4048b62b56d17647828233b8
Shekhawatbhawanisingh <shekhawatbhwanisingh49@gmail.com>
2025-06-24 10:05:01 +0530

    change cors origin

--- Backend/Index.js
+++ Backend/Index.js
@@ -16,7 +16,7 @@ connectDB();
 
 const app = express();
 const corsOptions = {
-  origin:  ['http://localhost:5173', 'https://majisa-solar.vercel.app'], // your frontend Vite dev server
+  origin:  ["*"], // your frontend Vite dev server
   credentials: true,
   
   methods: ["GET", "POST", "PUT", "DELETE"]
